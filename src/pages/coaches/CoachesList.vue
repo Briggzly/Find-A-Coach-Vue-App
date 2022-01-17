154f0ddfe04cf898800e4cb735d4bcfdf9261404
Kyson Briggs <kysonjbriggs@gmail.com>
2022-01-16 17:17:37 -0700

    feat: added navigation guard

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -18,7 +18,9 @@
             v-if="!isCoach && !isLoading && isLoggedIn"
             >Register as Coach</base-button
           >
-          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button link to="/auth?redirect=register" v-if="!isLoggedIn"
+            >Login to Register as Coach</base-button
+          >
         </div>
         <div v-if="isLoading">
           <base-spinner></base-spinner>
